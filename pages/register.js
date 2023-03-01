6973eefbe0f98d9fb82a26f281ffeaa7ed47b165
Ophélie Wask <ophelie.wask.bba@edhec.com>
2023-03-01 15:13:52 +0100

    fixed page register

--- pages/register.js
+++ pages/register.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { login } from "@/reducers/user";
 import Image from "next/image";
-import noImage from "../assets/no_image.jpeg";
+import noImage from "../public/no_image.jpeg";
 import { Google, Linkedin, Github } from "react-bootstrap-icons";
 import Input from "@/components/Input";
 import styles from "@/styles/onboarding.module.scss";
@@ -62,79 +62,79 @@ export default function Register() {
   };
 
   return (
-      <div className="container-fluid px-md-5">
-        <div className="col-lg-6">
-          <div className="d-flex justify-content-between align-items-center">
-            <h1 className="h2 py-3">Créer mon compte Linhub</h1>
-            <Link href="/login">Déjà un compte ?</Link>
-          </div>
-
-          <div>
-            <button>
-              <Google />
-              Google
-            </button>
-            <button>
-              <Linkedin />
-              Linkedin
-            </button>
-            <button>
-              <Github />
-              Github
-            </button>
-          </div>
-
-          <div className="d-flex justify-content-center align-items-center my-3">
-            <div className="border-bottom w-25"></div>
-            <div className="px-3">ou</div>
-            <div className="border-bottom w-25"></div>
-          </div>
-          <form onSubmit={handleSignUp}>
-            <Input
-              type="text"
-              label="Nom"
-              required={true}
-              value={signUpFirstname}
-              onChange={handleChange}
-              name="signUpFirstname"
-            />
-            <Input
-              type="text"
-              label="Prénom"
-              required={true}
-              value={signUpLastname}
-              onChange={handleChange}
-              name="signUpLastname"
-            />
-            <Input
-              type="email"
-              label="Email"
-              value={signUpEmail}
-              required={true}
-              onChange={handleChange}
-              name="signUpEmail"
-            />
-            <Input
-              type="password"
-              label="Mot de passe"
-              required={true}
-              value={signUpPassword}
-              onChange={handleChange}
-              name="signUpPassword"
-            />
-            <div className="text-end">
-              <button type="submit" className="btn btn-primary">
-                S'inscrire
-              </button>
-            </div>
-          </form>
+    <div className="container-fluid px-md-5">
+      <div className="col-lg-6">
+        <div className="d-flex justify-content-between align-items-center">
+          <h1 className="h2 py-3">Créer mon compte Linhub</h1>
+          <Link href="/login">Déjà un compte ?</Link>
         </div>
 
-        <Image
-          src={noImage}
-          alt="no-image"
-          className={`d-none d-lg-block ${styles.halfBg}`}
-        />
+        <div>
+          <button>
+            <Google />
+            Google
+          </button>
+          <button>
+            <Linkedin />
+            Linkedin
+          </button>
+          <button>
+            <Github />
+            Github
+          </button>
+        </div>
+
+        <div className="d-flex justify-content-center align-items-center my-3">
+          <div className="border-bottom w-25"></div>
+          <div className="px-3">ou</div>
+          <div className="border-bottom w-25"></div>
+        </div>
+        <form onSubmit={handleSignUp}>
+          <Input
+            type="text"
+            label="Nom"
+            required={true}
+            value={signUpFirstname}
+            onChange={handleChange}
+            name="signUpFirstname"
+          />
+          <Input
+            type="text"
+            label="Prénom"
+            required={true}
+            value={signUpLastname}
+            onChange={handleChange}
+            name="signUpLastname"
+          />
+          <Input
+            type="email"
+            label="Email"
+            value={signUpEmail}
+            required={true}
+            onChange={handleChange}
+            name="signUpEmail"
+          />
+          <Input
+            type="password"
+            label="Mot de passe"
+            required={true}
+            value={signUpPassword}
+            onChange={handleChange}
+            name="signUpPassword"
+          />
+          <div className="text-end">
+            <button type="submit" className="btn btn-primary">
+              S'inscrire
+            </button>
+          </div>
+        </form>
       </div>
+
+      <Image
+        src={noImage}
+        alt="no-image"
+        className={`d-none d-lg-block ${styles.halfBg}`}
+      />
+    </div>
   );
 }
