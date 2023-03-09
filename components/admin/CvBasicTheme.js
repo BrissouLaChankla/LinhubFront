395c0925414656de23054c755b5651ed38f1f9b2
Ophélie Wask <ophelie.wask.bba@edhec.com>
2023-03-09 11:38:57 +0100

    cv update

--- components/admin/CvBasicTheme.js
+++ components/admin/CvBasicTheme.js
@@ -22,96 +22,107 @@ export default function CvBasicTheme() {
   if (generalInfo.isError) return "Erreur";
 
   return (
-    <div className="App">
-      <div ref={ref}>
-        <div>
-          <h3>{generalInfo.data.user?.firstname}</h3>
-          <h3>{generalInfo.data.user?.lastname}</h3>
-          <img
-            src={generalInfo.data.user?.profilePicture}
-            alt="photo de profil"
-          />
-          <p>{generalInfo.data.user?.email}</p>
-          <p>{generalInfo.data.general?.address}</p>
-          <p>{generalInfo.data.general?.birthday}</p>
-          <p>{generalInfo.data.general?.experience}</p>
-          <p>{generalInfo.data.general?.headline}</p>
-          <p>{generalInfo.data.general?.description}</p>
-        </div>
-        <div>
-          <h3>Education</h3>
-          {generalInfo.data.educations?.map((education) => (
-            <div>
-              <p>{education.school}</p>
-              <p>{education.degree}</p>
-              <p>{education.fieldOfStudy}</p>
-              <p>{education.from}</p>
-              <p>{education.to}</p>
-              <p>{education.description}</p>
-            </div>
-          ))}
-        </div>
-        <div>
-          <h3>Experience</h3>
-          {generalInfo.data.experiences.map((experience) => (
-            <div>
-              <p>{experience.company}</p>
-              <p>{experience.title}</p>
-              <p>{experience.typeOfContract}</p>
-              <p>{experience.location}</p>
-              <p>{experience.locationString}</p>
-              <p>{experience.startMonthYear}</p>
-              <p>{experience.endMonthYear}</p>
-              <p>{experience.description}</p>
-            </div>
-          ))}
-        </div>
-        <div>
-          <h3>Skills</h3>
-          {generalInfo.data.skills[0]?.name.map((skill) => (
-            <div>
-              <p>{skill}</p>
-            </div>
-          ))}
-        </div>
-        <div>
-          <h3>Projects</h3>
-          {generalInfo.data.projects.map((project) => (
-            <div>
-              <p>{project.name}</p>
-              <p>{project.description}</p>
-              <p>{project.url}</p>
-              <p>{project.startDate}</p>
-              <p>{project.endDate}</p>
-            </div>
-          ))}
-        </div>
-        <div>
-          <h3>website</h3>
-          {generalInfo.data.websites.map((website) => (
-            <div>
-              <p>{website.name}</p>
-              <p>{website.url}</p>
-            </div>
-          ))}
+    <div className="container d-flex justify-content-center">
+      <div className="row">
+        <div ref={ref}>
           <div>
-            <h3>languages</h3>
-            {generalInfo.data.languages.map((language) => (
+            <div className="row align-items-center pb-4">
+              <div className="col-3">
+                <h3>{generalInfo.data.user?.firstname}</h3>
+                <h3>{generalInfo.data.user?.lastname}</h3>
+              </div>
+              <div className="col-9">
+                <img
+                  src={generalInfo.data.user?.profilePicture}
+                  alt="photo de profil"
+                />
+              </div>
+            </div>
+
+            <p>{generalInfo.data.user?.email}</p>
+            <p>{generalInfo.data.general?.address}</p>
+            <p>{generalInfo.data.general?.birthday}</p>
+            <p>{generalInfo.data.general?.experience}</p>
+            <p>{generalInfo.data.general?.headline}</p>
+            <p>{generalInfo.data.general?.description}</p>
+          </div>
+          <div>
+            <h3>Education</h3>
+            {generalInfo.data.educations?.map((education) => (
               <div>
-                <p>{language.name}</p>
-                <p>{language.proficiency}</p>
+                <p>{education.school}</p>
+                <p>{education.degree}</p>
+                <p>{education.fieldOfStudy}</p>
+                <p>{education.from}</p>
+                <p>{education.to}</p>
+                <p>{education.description}</p>
               </div>
             ))}
           </div>
+          <div>
+            <h3>Experience</h3>
+            {generalInfo.data.experiences.map((experience) => (
+              <div>
+                <p>{experience.company}</p>
+                <p>{experience.title}</p>
+                <p>{experience.typeOfContract}</p>
+                <p>{experience.location}</p>
+                <p>{experience.locationString}</p>
+                <p>{experience.startMonthYear}</p>
+                <p>{experience.endMonthYear}</p>
+                <p>{experience.description}</p>
+              </div>
+            ))}
+          </div>
+          <div className="row">
+            <h3>Skills</h3>
+            {generalInfo.data.skills[0]?.name.map((skill) => (
+              <div className="col-3">
+                <p>{skill}</p>
+              </div>
+            ))}
+          </div>
+          <div>
+            <h3>Projects</h3>
+            {generalInfo.data.projects.map((project) => (
+              <div>
+                <p>{project.name}</p>
+                <p>{project.description}</p>
+                <p>{project.url}</p>
+                <p>{project.startDate}</p>
+                <p>{project.endDate}</p>
+              </div>
+            ))}
+          </div>
+          <div>
+            <h3>website</h3>
+            {generalInfo.data.websites.map((website) => (
+              <div>
+                <p>{website.name}</p>
+                <p>{website.url}</p>
+              </div>
+            ))}
+            <div>
+              <h3>languages</h3>
+              {generalInfo.data.languages.map((language) => (
+                <div>
+                  <p>{language.name}</p>
+                  <p>{language.proficiency}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="text-center">
+          <Pdf targetRef={ref} filename="document.pdf">
+            {({ toPdf }) => (
+              <button onClick={toPdf} className="button">
+                Generate PDF
+              </button>
+            )}
+          </Pdf>
         </div>
       </div>
-      <Pdf targetRef={ref} filename="document.pdf">
-        {({ toPdf }) => (
-          <button onClick={toPdf} className="button">
-            Generate PDF
-          </button>
-        )}
-      </Pdf>
     </div>
   );
 }
